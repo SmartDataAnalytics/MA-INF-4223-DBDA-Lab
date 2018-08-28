699c0a88013a930a5e01fea00b2843700c9f8af6
SergeyKuptsov <sergeykuptsovde@gmail.com>
2018-08-28 15:10:41 +0200

    Updated repository

--- projects/project6/src/main/scala/axiom_extraction/Main.scala
+++ projects/project6/src/main/scala/axiom_extraction/Main.scala
@@ -10,6 +10,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 import scala.collection.mutable.ListBuffer
 import org.apache.spark.sql.functions._
 import org.apache.spark.ml.fpm.FPGrowth
+import net.sansa_stack.rdf.spark.model._
+import org.apache.jena.graph.NodeFactory
 
 
 object Main extends App {
@@ -24,17 +26,18 @@ object Main extends App {
       .getOrCreate()
     val resource = "test_input.nt"
     val path = getClass.getResource(resource).getPath()
-    val df: DataFrame = spark.read.rdf(Lang.NTRIPLES)(path)
+    val lang: Lang = Lang.NTRIPLES
+    
+    val df = spark.read.rdf(lang)(path)
     Console.println("===========Input DataFrames===========")
     df.show()
-    
     Console.println("===========Predicates===========")
     //selecting all distinct predicates
-    val distinctValuesDF = df.select("p").distinct
+    val distinctPredicatesDF = df.getPredicates().distinct
     //selecting all distinct instances
-    val distinctSubjectsDF = df.select("s").distinct
-    val distinctObjectsDF = df.select("o").distinct
-    distinctValuesDF.show()
+    val distinctSubjectsDF = df.getSubjects().distinct
+    val distinctObjectsDF = df.getObjects().distinct
+    distinctPredicatesDF.show()
     
     //all distinct instances in a list
     val instances = distinctSubjectsDF.union(distinctObjectsDF).distinct().rdd.map(r => r(0)).collect()
@@ -44,17 +47,13 @@ object Main extends App {
     
     var transactions : ListBuffer [String] = new ListBuffer()
     
-    
-    Console.println("===========Extendet Predicates===========")
-    //printing out distinct predicate values
-    distinctValuesDF.select("p").take((distinctValuesDF.count()).toInt).foreach(printWithNeg)    
-    
     //sampling the pair groups
     instances.foreach {x => getInstanceFilter(x,df,dfList)}
     
-    Console.println("===========Transactions Extracted===========")
+    Console.println("===========Transactions Extracted Symmetry===========")
     //extracting pairs and generating transactions
-    dfList.foreach(x=>transactions += extractTransactions(x))
+    //dfList.foreach(f=>f.show())
+    dfList.foreach(x=>transactions += extractTransactionsSymmetryAxiom(x))
     transactions.foreach(println)
     
     import spark.implicits._
@@ -69,10 +68,32 @@ object Main extends App {
     model.freqItemsets.show()
     
     // Display generated association rules.
-    Console.println("===========Association Rules===========")
+    Console.println("===========Association Rules Symmetry===========")
     model.associationRules.show()
-   
+    extractSymmetryAxiom(model.associationRules)
     
+    transactions.clear()
+    Console.println("===========Transactions Extracted Subsumption and Disj===========")
+    //extracting pairs and generating transactions
+    dfList.foreach(x=>transactions += extractTransactionsSubsumptionAndDisjointnessAxiom(x, distinctPredicatesDF))
+    transactions.foreach(println)
+    
+    import spark.implicits._
+    val datasetSub = spark.createDataset(transactions).map(t => t.split(" ")).toDF("items")
+    
+    
+    val fpgrowthSub = new FPGrowth().setItemsCol("items").setMinSupport(0.5).setMinConfidence(0.8)
+    val modelSub = fpgrowthSub.fit(datasetSub)
+    
+    Console.println("===========Frequent  Subsumption and Disj===========")
+    // Display frequent itemsets.
+    modelSub.freqItemsets.show()
+    
+    // Display generated association rules.
+    Console.println("===========Association Rules  Subsumption and Disj===========")
+    modelSub.associationRules.show()
+    extractSubsumptionDisjointnessAxiom(modelSub.associationRules)
+   
         
   }
 
@@ -81,12 +102,44 @@ object Main extends App {
 	      println(out)
 	}
 	
-	def printWithNeg(x:Any) {
-	  Console.println(x)	  
-	  Console.println("[not_"+x.toString().takeRight(x.toString().length()-1))
-	}
 	
-	def extractTransactions(df :DataFrame): String = {
+	def extractTransactionsSymmetryAxiom(df :DataFrame): String = {
+	  var count:Int = 0
+	  var transactions : ListBuffer[String] = new ListBuffer[String]
+    var outString: String = ""
+    var currObject: String = ""                        
+    count = df.count().toInt           
+    currObject = df.sort("o").first()(2).toString()
+    //outString = "(" + df.sort("o").first()(0).toString() + ":" + df.sort("o").first()(2).toString() + ") -> "
+    df.sort("o").collect.foreach{ x=>
+                    	    {                           	  
+                    	      outString += x(1) +" "
+                    	      outString += x(1).toString() +"^ "            	    
+                    	    }  
+                    	   
+                         }
+     return outString.take(outString.length()-1)
+	  }
+	
+		def extractSymmetryAxiom(df :DataFrame) {  
+    df.collect.foreach{ x=>
+                    	    {                           	  
+                    	      val ant: Seq[String] = x.getAs[Seq[String]]("antecedent")	  
+                    	      val con: Seq[String] = x.getAs[Seq[String]]("consequent")
+                    	      if(ant.size==1 && con.size==1)
+                    	      {
+                    	        val axiom: String = ant(0)
+                    	        if(con.contains(axiom+"^"))
+                    	        {
+                    	          Console.println("Sym("+axiom+")")
+                    	        }
+                    	      }
+                    	    }  
+                }
+	  }
+	
+	def extractTransactionsSubsumptionAndDisjointnessAxiom(df :DataFrame , predicates: DataFrame): String = {
+	  
 	  var count:Int = 0
 	  var transactions : ListBuffer[String] = new ListBuffer[String]
     var outString: String = ""
@@ -96,12 +149,38 @@ object Main extends App {
     //outString = "(" + df.sort("o").first()(0).toString() + ":" + df.sort("o").first()(2).toString() + ") -> "
     df.sort("o").collect.foreach{ x=>
                     	    {                           	  
-                    	      outString += x(1) +" "
-                    	      outString += "not_"+x(1).toString() +" "            	    
+                    	      outString += x(1) +" "           	    
                     	    }  
                     	   
                          }
-     return outString.take(outString.length()-1)
+	  predicates.collect.foreach(f=>{
+	    if (!(outString contains f(0).toString())){
+	    //  println("True: " + outString + " has no " + f(0).toString())
+	      outString += "not_"+ f(0).toString() + " "
+	    }
+	  })
+	   return outString.take(outString.length()-1)
+	  }
+	
+			def extractSubsumptionDisjointnessAxiom(df :DataFrame) {  
+    df.collect.foreach{ x=>
+                    	    {                           	  
+                    	      val ant: Seq[String] = x.getAs[Seq[String]]("antecedent")	  
+                    	      val con: Seq[String] = x.getAs[Seq[String]]("consequent")
+                    	      if(ant.size==1 && con.size==1)
+                    	      {
+                    	        val axiom: String = ant(0)
+                    	        if(con(0).contains("not_"))
+                    	        {
+                    	          Console.println("Disj("+axiom+","+con(0).replace("not_","")+")")
+                    	        }
+                    	        else
+                    	        {
+                    	          Console.println("Subsumes("+axiom+","+con(0)+")")
+                    	        }
+                    	      }
+                    	    }  
+                }
 	  }
 	
 	
