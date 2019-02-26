5aac368fd33a0ccb587c3ea36b87870618a30961
haseebjadoon <haseebjadoon0@gmail.com>
2019-02-27 04:57:19 +0500

    Suggested changes incorporated.

--- projects/project2/com.test.firstscalaprogram/src/com/test/firstscalaprogram/MainObject.scala
+++ projects/project2/com.test.firstscalaprogram/src/com/test/firstscalaprogram/MainObject.scala
@@ -8,199 +8,137 @@ import shapeless.ops.tuple.Length
 
 
 object MainObject {
-  
-  val pathOfDataFile : String = "E:\\tmp\\Spark_Workspace\\CommaSeperatedData.txt"
-  val appName : String = "Disdup"
-  val outputDirectory : String = "E:\\tmp\\Spark_Workspace\\output"
-  
-  
- def main(args:Array[String]) =
- { 
-   try
-   {  
-     NaiveDistinct()
-     //BlockBased()
-     //partitionEqually()
-   }
-   catch
-   {
-     case 
-     e : Exception => println(e)
-     e.printStackTrace()
-   }
- }
- 
- 
- def NaiveDistinct()
- {
-   try
-   {
-    val conf:SparkConf = new SparkConf().setAppName("Disdup").setMaster("local[*]")
-    val sc = SparkContext.getOrCreate(conf)
-    
-    val startTime = System.currentTimeMillis();
-    val textFile = sc.textFile("E:\\tmp\\Spark_Workspace\\CommaSeperatedData.txt", 2)
-    
-    ////////////////////Naive Distinct Function////////////////
-    
-    val testing = textFile.flatMap(record => 
-      {
-        record.split("\n")  
+
+  val pathOfDataFile: String = "C:\\Spark\\CommaSeperatedData.txt"
+  val appName: String = "Disdup"
+  val outputDirectory: String = "C:\\Spark\\Output"
+
+  def main(args: Array[String]) =
+    {
+      try {
+
+        var startTime = System.currentTimeMillis();
+        NaiveDistinct() //Naive-deduplication.
+        var endTime = System.currentTimeMillis();
+        var processingTimeN = endTime - startTime
+
+        startTime = System.currentTimeMillis();
+        BlockBased() //Blocking based on attribute.
+        endTime = System.currentTimeMillis();
+        var processingTimeB = endTime - startTime
+
+        startTime = System.currentTimeMillis();
+        partitionEqually() //equal partitioning.
+        endTime = System.currentTimeMillis();
+        var processingTimeC = endTime - startTime
+
+        println("Time taken Naive: " + processingTimeN)
+        println("Time taken Custom Paritioning: " + processingTimeC)
+        println("Time taken Blocking: " + processingTimeB)
+
+      } catch {
+        case e: Exception =>
+          println(e)
+
+          e.printStackTrace()
       }
-      ).distinct()
-    testing.saveAsTextFile("E:\\tmp\\Spark_Workspace\\output")
-    
-    ////////////////////////////////////////////////////////
-    
-    val endTime = System.currentTimeMillis();
-    val processingTime = endTime - startTime
-    println("Time taken: "+processingTime)
-   
-   }
-   catch
-   {
-     case 
-     e : Exception => println(e)
-     e.printStackTrace()
-   }
- }
- 
- 
- def partitionEqually()
- {
-   try
-   {
-    val conf:SparkConf = new SparkConf().setAppName(appName).setMaster("local[*]")
-    val sc = SparkContext.getOrCreate(conf)
-    
-    val startTime = System.currentTimeMillis();
-    val textFile = sc.textFile(pathOfDataFile)
-    
-    val wordCounts = getCounts(textFile)
-    
-    val totalPartitions = 2
-    val totalCount = textFile.count()
-    val partitionsForBlocks = getPartitionForBlock(totalCount, wordCounts, totalPartitions)
-    
-    val customPartitioner = new CustomPartitioner(totalPartitions, partitionsForBlocks)
-    
-    val deduplicatedData = textFile.flatMap(lines => lines.split("\n"))
-                 .map(word => (word, None))
-                 .partitionBy(customPartitioner)
-                 .reduceByKey((a, b) => a)
-    
-    deduplicatedData.saveAsTextFile(outputDirectory)
-   
-    val endTime = System.currentTimeMillis();
-    val processingTime = endTime - startTime
-    println("Time taken: "+processingTime)
-   }
-   catch
-   {
-     case 
-     e : Exception => println(e)
-     e.printStackTrace()
-   }
- }
- 
- def partitionEquallyAndBlocking()
- {
-   try
-   {
-    val conf:SparkConf = new SparkConf().setAppName(appName).setMaster("local[*]")
-    val sc = SparkContext.getOrCreate(conf)
-    
-    val startTime = System.currentTimeMillis();
-    val textFile = sc.textFile(pathOfDataFile)
-    
-    val wordCounts = getCounts(textFile)
-    
-    val totalPartitions = 2
-    val totalCount = textFile.count()
-    val partitionsForBlocks = getPartitionForBlock(totalCount, wordCounts, totalPartitions)
-    
-    val customPartitioner = new CustomPartitioner(totalPartitions, partitionsForBlocks)
-    
-    val deduplicatedData = textFile.flatMap(records => 
-      {
-        records.split("\n")
-        
-      }).map(word => (word, None))
+    }
+
+  def NaiveDistinct() {
+    try {
+      val spark = SparkSession.builder.appName("Disdup").master("local[*]").getOrCreate()
+      val sc = spark.sparkContext
+
+      val textFile = sc.textFile(pathOfDataFile, 2)
+
+      //Naive functionality
+      val testing = textFile.flatMap(record =>
+        {
+          record.split("\n")
+        }).distinct()
+      testing.saveAsTextFile(outputDirectory + "_Naive")
+
+    } catch {
+      case e: Exception =>
+        println(e)
+        e.printStackTrace()
+    }
+  }
+
+  def partitionEqually() {
+    try {
+      val spark = SparkSession.builder.appName("appName").master("local[*]").getOrCreate()
+      val sc = spark.sparkContext
+
+      val textFile = sc.textFile(pathOfDataFile)
+
+      val wordCounts = getCounts(textFile)
+
+      //get total number of partitions.
+      val totalPartitions = sc.getExecutorMemoryStatus.size
+      val totalCount = textFile.count()
+      val partitionsForBlocks = getPartitionForBlock(totalCount, wordCounts, totalPartitions)
+
+      val customPartitioner = new CustomPartitioner(totalPartitions, partitionsForBlocks)
+
+      val deduplicatedData = textFile.flatMap(lines => lines.split("\n"))
+        .map(word => (word, None))
         .partitionBy(customPartitioner)
         .reduceByKey((a, b) => a)
-    
-    deduplicatedData.saveAsTextFile(outputDirectory)
-   
-    val endTime = System.currentTimeMillis();
-    val processingTime = endTime - startTime
-    println("Time taken: "+processingTime)
-   }
-   catch
-   {
-     case 
-     e : Exception => println(e)
-     e.printStackTrace()
-   }
- }
 
-  def BlockBased()
- {
-   try
-   {
-    val conf:SparkConf = new SparkConf().setAppName(appName).setMaster("local[*]")
-    val sc = SparkContext.getOrCreate(conf)
-    
-    val startTime = System.currentTimeMillis();
-    val textFile = sc.textFile(pathOfDataFile)
-    
-    
-   val testing = textFile.flatMap(line => 
-     {
-       val records = line.split("\n")
-       val values = new Array[SingleRecord](records.length);
-       var i = 0;
-       for(record <- records)
-       {
-         values(i) = new SingleRecord(record) with Serializable
-         i+=1
-       }
-       values
-     }
-    )
-    .distinct()
-    
-   testing.saveAsTextFile(outputDirectory)
-    
-    val endTime = System.currentTimeMillis();
-    val processingTime = endTime - startTime
-    println("Time taken: "+processingTime)
-   
-   }
-   catch
-   {
-     case 
-     e : Exception => println(e)
-     e.printStackTrace()
-   }
-}
+      deduplicatedData.saveAsTextFile(outputDirectory + "_Custom")
 
-def getCounts(textFile : RDD[String]) : RDD[(String, Int)] =
-{
-       val counts = textFile.flatMap(line => line.split("\n"))
-                 .map(recordKey => 
-                   {
-                     (recordKey.takeWhile(_ != ','), 1)
-                   }
-                   )
-                 .reduceByKey(_ + _).sortBy(_._2)
-       
-       return counts
- }
- 
- def getPartitionForBlock(totalCount : Long, counts : RDD[(String, Int)], totalPartitions : Int) : HashMap[String, ListBuffer[BlockPartitionInfo]] =
- {
-   val partitionDecider = new PartitionDecider(totalCount, counts, totalPartitions)
-   partitionDecider.decidePartitions()
-   partitionDecider.partitionsForBlocks
- }
+    } catch {
+      case e: Exception =>
+        println(e)
+        e.printStackTrace()
+    }
+  }
+
+  def BlockBased() {
+    try {
+      val spark = SparkSession.builder.appName("BlockBased").master("local[*]").getOrCreate()
+      val sc = spark.sparkContext
+      val textFile = sc.textFile(pathOfDataFile)
+
+      val testing = textFile.flatMap(line =>
+        {
+          val records = line.split("\n")
+          val values = new Array[SingleRecord](records.length);
+          var i = 0;
+          for (record <- records) {
+            values(i) = new SingleRecord(record) with Serializable
+            i += 1
+          }
+          values
+        })
+        .distinct()
+
+      testing.saveAsTextFile(outputDirectory + "_Blocking")
+
+    } catch {
+      case e: Exception =>
+        println(e)
+        e.printStackTrace()
+    }
+  }
+
+  def getCounts(textFile: RDD[String]): RDD[(String, Int)] =
+    {
+      val counts = textFile.flatMap(line => line.split("\n"))
+        .map(recordKey =>
+          {
+            (recordKey.takeWhile(_ != ','), 1)
+          })
+        .reduceByKey(_ + _).sortBy(_._2)
+
+      return counts
+    }
+
+  def getPartitionForBlock(totalCount: Long, counts: RDD[(String, Int)], totalPartitions: Int): HashMap[String, ListBuffer[BlockPartitionInfo]] =
+    {
+      val partitionDecider = new PartitionDecider(totalCount, counts, totalPartitions)
+      partitionDecider.decidePartitions()
+      partitionDecider.partitionsForBlocks
+    }
 }
